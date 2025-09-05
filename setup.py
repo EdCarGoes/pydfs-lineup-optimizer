d775f72ae4eadff1182072bff95e94d3966e26a0
EdCarGoes <81978891+EdCarGoes@users.noreply.github.com>
2025-09-04 20:39:11 -0400

    setup change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-exec(open('pydfs_lineup_optimizer_ecg/version.py').read())
+exec(open('pydfs_lineup_optimizer/version.py').read())
 
 
 def read(fname):
